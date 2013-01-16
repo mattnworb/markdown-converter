d29acb2d82b3f28182e59d2c40fa9676951784ea
Matt Brown <octavius@gmail.com>
2013-01-16 12:31:16 -0500

    fix inconsistent indents and remove superfluous code

--- markdown_converter.py
+++ markdown_converter.py
@@ -19,7 +19,6 @@ def convertText(c):
     # replace inline code
     c = re.sub(r'`(.*?)`', r'*\1*', c)
 
-    # print c
     c = c.split('\n')
 
     words = []
@@ -38,55 +37,53 @@ def convertText(c):
     isList = 0
 
     for l in c:
-      if 0 == 0:
         k = l
         if l[0:1]=='*':
-          isList = 1
+            isList = 1
         if l == '':
-          isList = 0
+            isList = 0
 
         if l[0:1] == '>':
-          if isQuote==0:
-            k = '{quote}\n'+k[1:]
-            isQuote = 1
-          else:
-            k = k[1:]
+            if isQuote==0:
+                k = '{quote}\n'+k[1:]
+                isQuote = 1
+            else:
+                k = k[1:]
 
 
         if isList == 0:
-          if isCode == 1:
-            if l[0:1] == ' ' or l[0:1]=='\t':
-              k = k[indent:]  
+            if isCode == 1:
+                if l[0:1] == ' ' or l[0:1]=='\t':
+                    k = k[indent:]  
+                else:
+                    k = '{code}\n'+k
+                    isCode = 0
+                    indent = -1
             else:
-              k = '{code}\n'+k
-              isCode = 0
-              indent = -1
-          else:
-            if l[0:1]==' ' or l[0:1]=='\t':
-              indent = len(k)-len(k.lstrip())
-              k = '{code}\n'+k[indent:]              
-              isCode = 1
+                if l[0:1]==' ' or l[0:1]=='\t':
+                    indent = len(k)-len(k.lstrip())
+                    k = '{code}\n'+k[indent:]              
+                    isCode = 1
         else:
-           if l[0:4]=='\t\t\t*':
-             k = '****' + l[4:]
-           if l[0:3]=='\t\t*':
-             k = '***' + l[3:]
-           if l[0:2]=='\t*':
-             k = '**' + l[2:]
+            if l[0:4]=='\t\t\t*':
+                k = '****' + l[4:]
+            if l[0:3]=='\t\t*':
+                k = '***' + l[3:]
+            if l[0:2]=='\t*':
+                k = '**' + l[2:]
 
         for w in words:
-          # print l[:len(w[0])]
-          if l[:len(w[0])] == w[0]:
-            k = w[1]+l[len(w[0]):]
+            if l[:len(w[0])] == w[0]:
+                k = w[1]+l[len(w[0]):]
 
 
         if l[0:1] != '>' and isQuote == 1:
-          k = '{quote}\n'+k
-          isQuote = 0
+            k = '{quote}\n'+k
+            isQuote = 0
 
 
         if l[0:3] != '| -':
-          newContent.append(k)
+            newContent.append(k)
 
     return '\n'.join(newContent)
 
